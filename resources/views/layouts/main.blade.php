2a5eb03bbd08e5e07e07d60d045a8826e33efee5
Cristian <ctiry01@gmail.com>
2020-07-21 22:25:01 +0200

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Generador de contraseñas online</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div class="content">
+        @yield('mainContent')
+    </div>
+</div>
+</body>
+</html>
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+
+@section('mainContent')
+    <div>
+        <h1>Tu generador de contraseñas gratis</h1>
+        <h3>Tu clave segura en un solo click</h3>
+        <a href="" class="btn btn-primary">Generar contraseña</a>
+
+    </div>
+@endsection
